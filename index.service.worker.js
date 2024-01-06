623549e1ef480b510cf07f08c2fa3750bcec912f
Alexey Korepanov <alexkorep@gmail.com>
2024-01-06 19:00:13 +0100

    Updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1704563307|68987250995";
+const CACHE_VERSION = "1704563999|69679366946";
 const CACHE_PREFIX = "AstroSweep-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
